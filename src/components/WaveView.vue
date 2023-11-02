4457ed16eac3c84d114c91864489728e196cf0c7
dingxiuan <dingxiuan@163.com>
2023-11-02 18:36:43 +0800

    测试12导

--- src/components/WaveView.vue
+++ src/components/WaveView.vue
@@ -3,27 +3,26 @@
     <div
       id="container"
       class="wvParent"
-      style="width: 100%; height: 800px; background-color: white"
+      style="width: 100%; height: 780px; background-color: white"
     >
       <div
         id="wvBg"
         class="wvBg"
         style="width: 100%; height: 100%; background-color: white"
       ></div>
-      <div
-        id="wv"
-        class="wv"
-        style="width: 100%; height: 100%; alpha: true"
-      ></div>
+      <div id="wv" class="wv">
+        <span id="wv1" style="width: 50%; height: 100%; alpha: true">111111</span>
+        <span id="wv2" style="width: 50%; height: 100%; alpha: true">222222</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-import { parseZCenter } from '../libs/device/zcenter'
-import { mqtt } from '../libs/mqtt/mqtt'
 import { utils } from '../libs/core'
+import { mqtt } from '../libs/mqtt/mqtt'
+import { parseZCenter } from '../libs/device/zcenter'
 
 import {
   createCanvasGridBG,
@@ -34,7 +33,8 @@ import {
 
 const row = 6
 const column = 2
-let wv: WaveView
+let wv1: WaveView
+let wv2: WaveView
 
 const mqttClient = new mqtt.Client(<mqtt.MqttOptions>{
   autoReconnectInterval: 5000,
@@ -50,20 +50,18 @@ const mqttClient = new mqtt.Client(<mqtt.MqttOptions>{
 let subscriber = <mqtt.MqttSubscriber>{
   onMessage(client, topic, msg) {
     // console.log(`${client.clientId}, 接收到mqtt消息`, topic, msg)
-    let packet = parseZCenter(msg.payloadBytes)
 
-    let array = new Array<number[]>(row * column)
-    for (let i = 0; i < row * column; i++) {
-      array[i] = [...packet.ecgList]
-    }
-    wv.push(...array)
-
-    //console.log(`${client.clientId}, subscriber1 接收到mqtt消息`, topic, msg);
-    console.log(
-      `${client.clientId}, ${topic}, sn: ${
-        packet.packageSn
-      }, time: ${utils.dateFmt(packet.time * 1000)}`
-    )
+    // let packet = parseZCenter(msg.payloadBytes)
+    // let array = new Array<number[]>(row * column)
+    // for (let i = 0; i < row * column; i++) {
+    //   array[i] = [...packet.ecgList]
+    // }
+    // wv.push(...array)
+    // console.log(
+    //   `${client.clientId}, ${topic}, sn: ${
+    //     packet.packageSn
+    //   }, time: ${utils.dateFmt(packet.time * 1000)}`
+    // )
 
     // try {
     //   data.value.message = `time: ${utils.dateFmt(packet.time * 1000)}, sn: ${
@@ -72,6 +70,25 @@ let subscriber = <mqtt.MqttSubscriber>{
     // } catch (err) {
     //   console.error(err)
     // }
+
+    const packet = JSON.parse(msg.payloadString)
+    console.log(packet)
+    let array1 = new Array<number[]>(row * (column / 2))
+    array1[0] = [...(packet.I ? packet.I : packet.i)]
+    array1[1] = [...(packet.II ? packet.II : packet.iI)]
+    array1[2] = [...(packet.III ? packet.III : packet.iII)]
+    array1[3] = [...packet.aVR]
+    array1[4] = [...packet.aVL]
+    array1[5] = [...packet.aVF]
+    wv1.push(...array1)
+    let array2 = new Array<number[]>(row * (column / 2))
+    array2[0] = [...(packet.V1 ? packet.V1 : packet.v1)]
+    array2[1] = [...(packet.V2 ? packet.V2 : packet.v2)]
+    array2[2] = [...(packet.V3 ? packet.V3 : packet.v3)]
+    array2[3] = [...(packet.V4 ? packet.V4 : packet.v4)]
+    array2[4] = [...(packet.V5 ? packet.V5 : packet.v5)]
+    array2[5] = [...(packet.V6 ? packet.V6 : packet.v6)]
+    wv2.push(...array2)
   },
   onConnected(client) {
     console.log(`${client.clientId}, 客户端连接成功`)
@@ -86,23 +103,35 @@ let subscriber = <mqtt.MqttSubscriber>{
     console.log(`${client.clientId}, 消息送达`, msg)
   }
 }
-mqttClient.subscribe(subscriber, 'hardware/11000138') // 订阅
+// mqttClient.subscribe(subscriber, 'hardware/11000139') // 订阅
+mqttClient.subscribe(subscriber, '/hardware/boying12/00195D244F11') // 订阅
 
 onMounted(() => {
   setTimeout(() => {
     let container = document.getElementById('container')
-    container?.setAttribute('width', document.body.clientWidth.toString())
-    container?.setAttribute('height', document.body.clientHeight.toString())
+    container?.setAttribute('width', `${document.body.clientWidth * 0.8}`)
+    container?.setAttribute('height', `${document.body.clientHeight * 0.8}`)
 
     createCanvasGridBG(document.getElementById('wvBg') as any)
-    wv = createCanvasWaveView(
-      document.getElementById('wv') as any,
+    wv1 = createCanvasWaveView(
+      document.getElementById('wv1') as any,
       row,
-      column,
+      column / 2,
       {
         ...DEFAULT_OPTS,
-        scaleRatio: 0.5,
-        step: 1.0,
+        scaleRatio: 0.8,
+        step: 0.8,
+        lineWidth: 1.0
+      }
+    )
+    wv2 = createCanvasWaveView(
+      document.getElementById('wv2') as any,
+      row,
+      column / 2,
+      {
+        ...DEFAULT_OPTS,
+        scaleRatio: 0.8,
+        step: 0.8,
         lineWidth: 1.0
       }
     )
